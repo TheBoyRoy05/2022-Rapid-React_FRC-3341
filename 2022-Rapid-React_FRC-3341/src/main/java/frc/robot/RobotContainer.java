1b9270d0821bb5f4a6f8e8043d40738fe5afb421
TheBoyRoy05 <issacroy05@gmail.com>
2022-02-11 20:49:04 -0800

    why do I have literally update each and every thing

--- 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/RobotContainer.java
+++ 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,14 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.Rotate;
+import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,6 +21,11 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+  private Climber climber;
+  private Rotate up;
+  private Rotate down;
+  private Rotate zero;
+  private Joystick joy;
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
@@ -25,6 +34,10 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
+    climber = new Climber();
+    up = new Rotate(climber, 1, Constants.armAngles.up);
+    down = new Rotate(climber, 1, Constants.armAngles.down);
+    zero = new Rotate(climber, 1, Constants.armAngles.zero);
     configureButtonBindings();
   }
 
@@ -34,7 +47,15 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    JoystickButton zeroButton = new JoystickButton(joy, 3);
+    JoystickButton upButton = new JoystickButton(joy, 4);
+    JoystickButton downButton = new JoystickButton(joy, 5);
+
+    zeroButton.whenPressed(zero);
+    upButton.whenPressed(up);
+    downButton.whenPressed(down);
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
